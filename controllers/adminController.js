7d36ae7bff375173bc000789a89927d695fba728
Ritik Umang <ritik.umang.bvp@gmail.com>
2024-10-17 22:03:12 +0530

    commit-5

---
+++ controllers/adminController.js
@@ -0,0 +1,36 @@
+import { request } from "express";
+
+// API for Adding Doctor
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+    const imageFile = req.file;
+
+    console.log(
+      {
+        name,
+        email,
+        password,
+        speciality,
+        degree,
+        experience,
+        about,
+        fees,
+        address,
+      },
+      imageFile
+    );
+  } catch (error) {}
+};
+
+export { addDoctor };
