7bb7fa280cace8b42ce14a5d4325566f08f42f9f
Ritik Umang <ritik.umang.bvp@gmail.com>
2024-10-20 22:22:22 +0530

    commit-4

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,6 +3,7 @@ import validator from 'validator';
 import bycrypt from 'bcrypt';
 import {v2 as cloudinary} from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
+import jwt from 'jsonwebtoken'
 
 // API for Adding Doctor
 const addDoctor = async (req, res) => {
@@ -86,4 +87,25 @@ const addDoctor = async (req, res) => {
   }
 };
 
-export { addDoctor };
+// API for admin login
+const loginAdmin = async(req,res) => {
+  try{
+
+    const {email, password} =req.body
+
+    if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+
+      const token = jwt.sign(email+password,process.env.JWT_SECRET)
+      res.json({success:true,token})
+
+    } else {
+      res.json({success:false,message:"Invalid credentials"})
+    }
+
+  }catch(error) {
+    console.log(error)
+    res.json({success:false,message:error.message})
+  }
+}
+
+export { addDoctor,loginAdmin };
